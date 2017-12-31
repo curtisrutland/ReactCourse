264e360867e284a40f707f619d73b1facdb6f13d
Curtis Rutland <curtisrutland@gmail.com>
2017-12-31 12:36:03 -0600

    added working backdrop to sidedrawer, working dismiss

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Logo from "../../ui/Logo/Logo";
 import NavItems from "../NavItems/NavItems";
+import Backdrop from "../../ui/Backdrop/Backdrop";
 import classes from "./SideDrawer.css";
 
 const SideDrawer = props => {
+    const classNames = `${classes.SideDrawer} ${props.open ? classes.Open : classes.Close}`;
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}><Logo /></div>
-            <nav>
-                <NavItems />
-            </nav>
-        </div>
+        <Fragment>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={classNames}>
+                <div className={classes.Logo}><Logo /></div>
+                <nav>
+                    <NavItems />
+                </nav>
+            </div>
+        </Fragment>
     );
 }
 
